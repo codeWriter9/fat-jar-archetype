457b7a820b26e49b8023222f02ce685c342aed25
Sanjay Ghosh <nerdsubhankar2006@gmail.com>
2025-04-16 19:46:52 +0530

    Add Hello World

---
+++ addcommitpush.sh
@@ -0,0 +1,1 @@
+git add .;git commit -m "Add Hello World";git push https://$USERNAME:$PASSWORD@github.com/codeWriter9/fat-jar-archetype.git main
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,25 @@
+package $package;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        log.info("Running Tests!");
+        assertTrue( true );
+        log.info("Tests Completed Successfully!");
+    }
+}
